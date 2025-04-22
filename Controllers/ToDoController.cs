9fe6f5c24014c43197a5138bdee6f45d301439b5
Lilian-Haki <irungulilianhaki@gmail.com>
2025-04-22 14:12:14 +0300

    Add project files.

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TodoController : ControllerBase
+{
+    private readonly TodoContext _context;
+
+    public TodoController(TodoContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodos()
+    {
+        return await _context.TodoItems.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItem>> GetTodo(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+        if (item == null) return NotFound();
+        return item;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> CreateTodo(TodoItem item)
+    {
+        _context.TodoItems.Add(item);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetTodo), new { id = item.Id }, item);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateTodo(int id, TodoItem item)
+    {
+        if (id != item.Id) return BadRequest();
+        _context.Entry(item).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodo(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+        if (item == null) return NotFound();
+        _context.TodoItems.Remove(item);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ Data/ToDoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ToDoApi.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+    : base(options) { }
+
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+}
+
